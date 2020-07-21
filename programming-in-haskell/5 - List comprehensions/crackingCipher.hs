f5a1dee729d68b52a286b5836f061f561c34cdf7
zeus <gauthamv93@yahoo.com>
2020-07-20 21:07:49 -0600

    cracking the cipher

---
+++ programming-in-haskell/5 - List comprehensions/crackingCipher.hs
@@ -0,0 +1,52 @@
+import Data.Char
+
+freqTable = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0,
+             0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
+             6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+
+count :: Char -> [Char] -> Int
+count x xs = length [x | x' <- xs, x' == x]
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+lowers :: [Char] -> Int
+lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
+
+freqs :: [Char] -> [Float]
+freqs xs = [percent (count x xs) m | x <- ['a'..'z']]
+            where m = lowers xs
+
+chiSqr :: [Float] -> [Float] -> Float
+chiSqr os es = sum [((o - e)^2) / e | (o, e) <- zip os es]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
+
+-- /////////////////////////////////////////////////////////
+-- Learn to import in haskell
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char
+int2let n = chr (ord 'a' + n)
+
+shift :: Int -> Char -> Char
+shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+          | otherwise = c
+
+encode :: Int -> [Char] -> [Char]
+encode n xs = [shift n x | x <- xs]
+
+position :: Eq a => a -> [a] -> [Int]
+position x xs = [i | (x', i) <- zip xs [0..], x' == x]
+
+-- ////////////////////////////////////////////////////////
+
+crack :: [Char] -> [Char]
+crack xs = encode (-factor) xs
+            where 
+                factor = head (position (minimum chiTab) chiTab)
+                chiTab = [chiSqr (rotate n table') freqTable | n <- [0..25]]
+                table' = freqs xs
