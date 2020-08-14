a22907788e3c4346be2770f20baa59f625580b96
zeus <gauthamv93@yahoo.com>
2020-08-13 23:37:57 -0600

    tautology checker

---
+++ programming-in-haskell/8 - Declaring types and classes/TautologyChecker.hs
@@ -0,0 +1,65 @@
+-- Chapter Exercise section 8.6
+-- Tautology checker
+-- For expressions: 
+-- A ∧ ¬A
+-- (A ∧ B) ⇒ A
+-- A ⇒ (A ∧ B)
+-- (A ∧ (A ⇒ B)) ⇒ B
+
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Imply Prop Prop
+
+-- A ∧ ¬A
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+-- (A ∧ B) ⇒ A
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+-- A ⇒ (A ∧ B)
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+-- (A ∧ (A ⇒ B)) ⇒ B
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+vars :: Prop -> [Char]
+vars (Const _) = []
+vars (Var x) = [x]
+vars (Not x) = vars x
+vars (And x y) = vars x ++ vars y
+vars (Imply x y) = vars x ++ vars y
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+            where bss = bools (n - 1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) xs
+
+type Assoc k v = [(k, v)]
+type Subst = Assoc Char Bool
+
+substs :: Prop -> [Subst]
+substs prop = map (zip vs) (bools (length vs))
+                where vs = rmdups (vars prop)
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k', v) <- t, k == k']
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const x) = x
+eval s (Var x) = find x s
+eval s (Not x) = not (eval s x)
+eval s (And x y) = (eval s x) && (eval s y)
+eval s (Imply x y) = (eval s x) <= (eval s y)
+
+isTaut :: Prop -> Bool
+isTaut prop = and [eval s prop | s <- substs prop]
