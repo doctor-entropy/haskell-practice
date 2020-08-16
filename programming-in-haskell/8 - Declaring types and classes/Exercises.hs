debc3eea5419581e08b629b985fda7c824139b9c
zeus <gauthamv93@yahoo.com>
2020-08-15 18:02:38 -0600

    More exercise problems

--- programming-in-haskell/8 - Declaring types and classes/Exercises.hs
+++ programming-in-haskell/8 - Declaring types and classes/Exercises.hs
@@ -33,3 +33,27 @@ occurs' x (Node l y r) = case compare x y of
 t :: Tree Int
 t = Node (Node (Leaf 1) 3 (Leaf 4)) 5
          (Node (Leaf 6) 7 (Leaf 9))
+
+-- 3
+-- In file BalancedBinaryTree.hs
+
+-- 4
+-- In file ToBalance.hs
+
+-- 5
+data Expr = Val Int | Add Expr Expr
+                deriving Show
+
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+folde f g (Val x) = f x
+folde f g (Add x y) = g (folde f g x) (folde f g y)
+
+-- 6
+eval :: Expr -> Int
+eval e = folde id (+) e
+
+size :: Expr -> Int
+size e = folde (^0) (+) e
+-- or
+size' :: Expr -> Int
+size' e = folde (\_ -> 1) (+) e
