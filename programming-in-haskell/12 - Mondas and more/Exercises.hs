fe51364cfa0034a4f99f4c23647865f3bde839fb
zeus <gauthamv93@yahoo.com>
2021-01-31 14:58:57 -0700

    More chapter 12 exercise practice

--- programming-in-haskell/12 - Mondas and more/Exercises.hs
+++ programming-in-haskell/12 - Mondas and more/Exercises.hs
@@ -25,5 +25,33 @@ tree = Node (Node (Leaf) 2 (Leaf)) 1 (Node (Leaf) 3 (Leaf))
 --   (<*>) :: (a -> b -> c) -> (a -> b) -> (b -> c)
 --   g <*> h = \x -> g x (h x)
 
--- 4
+-- Trying again (Qs. 2 and 3)
+-- instance Functor ((->) a) where
+--   fmap :: (b -> c) -> (a -> b) -> (a -> c)
+--   fmap = (.)
 
+-- instance Functor ((-> a) a) => Applicative ((-> a) a) where
+--   pure :: (a -> b) -> (a -> b)
+--   pure = const
+
+--   (<*>) :: (a -> b -> c) -> (a -> b) -> (b -> c)
+--   f <*> g = (\x -> f x (g x))
+
+-- 4
+newtype ZipList a = Z [a] deriving Show
+
+instance Functor ZipList where
+  fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) = Z (fmap g xs)
+
+instance Applicative ZipList where
+  pure :: a -> ZipList a
+  pure x = Z (repeat x)
+
+  (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+  (Z gs) <*> (Z xs) = Z [ g x | (g, x) <- zip gs xs]
+
+-- 6
+-- instance Applicative ((-> a) a) => Monad ((-> a) a) where
+--   (>>=) :: (a -> b) -> ((a -> b) -> ( b-> c)) -> (b -> c)
+--   f >>= g = g . f
