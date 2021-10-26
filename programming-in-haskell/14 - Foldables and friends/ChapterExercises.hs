0a08ae5569a55dba2d852f605f7c5386fd971258
groot <gauthamv93@yahoo.com>
2021-10-26 12:14:25 +0530

    Monoids Chapter practice

--- programming-in-haskell/14 - Foldables and friends/ChapterExercises.hs
+++ programming-in-haskell/14 - Foldables and friends/ChapterExercises.hs
@@ -13,4 +13,40 @@ instance Monoid [a] where
     mappend = (++)
 
 instance Monoid a => Monoid (Maybe a) where
-    
+    -- mempty :: Maybe a
+    mempty = Nothing
+
+    -- mappend :: Maybe a -> Maybe a -> Maybe a
+    Nothing `mappend` my      = my
+    mx      `mappend` Nothing = mx
+    Just x  `mappend` Just y  = Just (x `mappend` y)  
+
+newtype Sum a = Sum a
+                (deriving Show, Eq, Ord, Read)
+
+getSum :: Sum a -> a
+getSum (Sum x) = x
+
+instance Num a => Monoid (Sum a) where
+    -- mempty :: Sum a
+    mempty = Sum 0
+
+    -- mappend :: Sum a -> Sum a -> Sum a
+    Sum x `mappend` Sum y = Sum (x + y)
+
+newtype Product a = Product a
+                        deriving (Show, Eq, Ord, Read)
+
+getProduct :: Product a -> a
+getProduct (Product x) = x
+
+instance Num a => Monoid (Num a) where
+    -- mempty :: Product a
+    mempty = Product 1
+
+    -- mappend :: Product a -> Product a -> Product a
+    Product x `mappend` Product y = Product (x * y)
+
+
+-- Foldables
+
