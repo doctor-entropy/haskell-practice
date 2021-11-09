065d254922a9af6f1630e36bdfcde0aff18a723b
groot <gauthamv93@yahoo.com>
2021-11-09 23:09:41 +0530

    A few monads more

---
+++ learn-you-haskell/13 - For a Few Monads More/logNumber.hs
@@ -0,0 +1,45 @@
+import Control.Monad.Writer  
+  
+logNumber :: Int -> Writer [String] Int  
+logNumber x = writer (x, ["Got number: " ++ show x])  
+  
+multWithLog :: Writer [String] Int  
+multWithLog = do  
+    a <- logNumber 3  
+    b <- logNumber 5  
+    return (a*b)
+
+-- Adding logging to programs
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+    | b == 0 = do
+        tell ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        gcd' b (a `mod` b)
+
+-- Difference lists
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs ++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+    (DiffList f) <> (DiffList g) = DiffList (\xs -> f (g xs))
+
+instance Monoid (DiffList a) where  
+    mempty = DiffList (\xs -> [] ++ xs)
+
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse a b
+    | b == 0 = do
+        tell (toDiffList ["Finished with " ++ show a])
+        return a
+    | otherwise = do
+        result <- gcdReverse b (a `mod` b)
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+        return result
